b7508b47ccb4b8ab583639ae47b3e1db7da584b4
Harrison Zuleta Montoya <harrison.zmontoya@gmail.com>
2026-03-15 13:07:21 -0500

    feat: analysis count error encoding on scraped columns(job_via,company_name,job_title)

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from ..utils.analysis import payment_analysis
+from utils.analysis import payment_analysis, encoding_errors, aggregation_analysis
 
 if __name__ == '__main__':
     payment_analysis()
+    print('=' * 70)
+    aggregation_analysis()
+    encoding_errors()
